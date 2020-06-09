1fa3f9f7e6652428d2cc26a8eecdac9ef1b7d183
moloch-- <875022+moloch--@users.noreply.github.com>
2020-06-08 22:48:37 -0500

    Swapped flags around

--- internal/api-server/cmd.go
+++ internal/api-server/cmd.go
@@ -1,5 +1,22 @@
 package apiserver
 
+/*
+	---------------------------------------------------------------------
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	----------------------------------------------------------------------
+*/
+
 import (
 	"fmt"
 	"os"
@@ -43,15 +60,16 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
+	versionCmd.PersistentFlags().BoolP(detailsFlagStr, "d", false, "detailed version info")
 	rootCmd.AddCommand(versionCmd)
 
-	rootCmd.PersistentFlags().StringP(hostFlagStr, "H", "", "Bind host")
+	rootCmd.PersistentFlags().StringP(hostFlagStr, "b", "", "Bind host")
 	rootCmd.PersistentFlags().Uint16P(portFlagStr, "p", 8888, "Bind port")
 
-	rootCmd.PersistentFlags().StringP(jsonFlagStr, "j", "", "JSON data set file")
-	rootCmd.PersistentFlags().StringP(userIndexFlagStr, "u", "", "User index file")
-	rootCmd.PersistentFlags().StringP(emailIndexFlagStr, "e", "", "Email index file")
-	rootCmd.PersistentFlags().StringP(domainIndexFlagStr, "d", "", "Domain index file")
+	rootCmd.PersistentFlags().StringP(jsonFlagStr, "J", "", "JSON data set file")
+	rootCmd.PersistentFlags().StringP(userIndexFlagStr, "U", "", "User index file")
+	rootCmd.PersistentFlags().StringP(emailIndexFlagStr, "E", "", "Email index file")
+	rootCmd.PersistentFlags().StringP(domainIndexFlagStr, "D", "", "Domain index file")
 
 	rootCmd.PersistentFlags().BoolP(tlsFlagStr, "s", false, "Enable TLS")
 	rootCmd.PersistentFlags().StringP(certFlagStr, "c", "", "TLS certificate")
